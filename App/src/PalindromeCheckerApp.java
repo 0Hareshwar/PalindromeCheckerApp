b5c54c39030283e1402d8dc2c4afe302cf7f58cb
0Hareshwar <hareshwar4927@gmail.com>
2026-03-02 09:31:29 +0530

    ADDED UC2

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,14 @@
 public class PalindromeCheckerApp {
-    public static void main(string[] args){
-        system.out.println("welcome to palindrome checker app system");
-    }
-}
+    public static void main(String[] args) {
+        String word = "madam";
+        String reversed = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a palindrome.");
+        }else{
+                    System.out.println(word + " is not a palindrome.");
+        }
+            }
+        }
